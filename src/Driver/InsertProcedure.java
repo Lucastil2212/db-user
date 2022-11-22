a59eb7908ee3b9c620b5f3db147caf05422a81bd
lucastil2212 <lucastil622@gmail.com>
2022-11-21 19:44:13 -0500

    implemented inserts

--- src/Driver/InsertProcedure.java
+++ src/Driver/InsertProcedure.java
@@ -1,21 +1,28 @@
 package Driver;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
 
 public class InsertProcedure {
-    //Statement insertSchedule = null;
-    // CallableStatement insertPilot = null;
-    // Statement insertPlane = null;
+    private static PreparedStatement insertSchedule = null;
+    private static PreparedStatement insertPilot = null;
+    private static PreparedStatement insertPlane = null;
     private static PreparedStatement insertPassenger = null;
-    // Statement insertLocation = null;
-    // Statement insertRoute = null;
-    // Statement insertStatus = null;
+    private static PreparedStatement insertLocation = null;
+    private static PreparedStatement insertRoute = null;
+    private static PreparedStatement insertStatus = null;
 
     public static void createProcedures(Connection con) throws SQLException{    
 
+        insertSchedule = con.prepareStatement("{call dbo.add_schedule(?, ?, ?, ?, ?, ?)}");
+        insertPilot = con.prepareStatement("{call dbo.add_pilot(?, ?)}");
+        insertPlane = con.prepareStatement("{call dbo.add_plane(?, ?)}");
         insertPassenger = con.prepareStatement("{call dbo.add_passenger(?, ?)}");
+        insertLocation = con.prepareStatement("{call dbo.add_location(?, ?)}");
+        insertRoute = con.prepareStatement("{call dbo.add_route(?, ?)}");
+        insertStatus = con.prepareStatement("{call dbo.add_status(?, ?)}");
     }
 
     public static void addPassenger(String name, String email){
@@ -32,4 +39,93 @@ public class InsertProcedure {
         
     }
 
+    public static void addSchedule(int planeId, int routeId, Date startTime, Date endTime, int pilotId, String status){
+        try{
+            insertSchedule.setInt(1, planeId);
+            insertSchedule.setInt(2, routeId);
+            insertSchedule.setDate(3, startTime);
+            insertSchedule.setDate(4, endTime);
+            insertSchedule.setInt(5, pilotId);
+            insertSchedule.setString(6, status);
+
+
+            insertSchedule.execute();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println(e.toString());
+        }
+        
+    }
+
+    public static void addPilot(String name, Float salary){
+        try{
+            insertPilot.setString(1, name);
+            insertPilot.setFloat(2, salary);
+
+            insertPilot.execute();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println(e.toString());
+        }
+        
+    }
+
+    public static void addPlane(String model, String manufacturer, int capacity){
+        try{
+            insertPlane.setString(1, model);
+            insertPlane.setString(2, manufacturer);
+            insertPlane.setInt(3, capacity);
+
+            insertPlane.execute();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println(e.toString());
+        }
+        
+    }
+
+    public static void addRoute(int start_location, int end_location){
+        try{
+            insertRoute.setInt(1, start_location);
+            insertRoute.setInt(2, end_location);
+
+            insertRoute.execute();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println(e.toString());
+        }
+        
+    }
+
+    public static void addLocation(String name, int capacity){
+        try{
+            insertLocation.setString(1, name);
+            insertLocation.setInt(3, capacity);
+
+            insertLocation.execute();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println(e.toString());
+        }
+        
+    }
+
+    public static void addStatus(String status, String description){
+        try{
+            insertStatus.setString(1, status);
+            insertStatus.setString(2, description);
+
+            insertStatus.execute();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println(e.toString());
+        }
+        
+    }
 }
---
+++ src/scripts/create_tables.sql
@@ -0,0 +1,69 @@
+BEGIN
+	CREATE TABLE location 
+	( location_id int IDENTITY(1, 1),
+	  name varchar(30) NOT NULL,
+	  capacity int NOT NULL check (capacity > 0),
+	  primary key(location_id));
+
+	CREATE TABLE route
+	( route_id int IDENTITY(1, 1),
+	  start_location_id int NOT NULL,
+	  end_location_id int NOT NULL,
+	  foreign key(start_location_id) references location(location_id),
+	  foreign key(end_location_id) references location(location_id),
+	  primary key(route_id));
+
+	CREATE TABLE status
+	( status varchar(20) ,
+	  description varchar(50) NOT NULL,
+	  primary key(status));
+
+	CREATE TABLE passenger
+	( email varchar(25),
+	  name varchar(25) NOT NULL,
+	  primary key(email));
+
+	CREATE TABLE pilot
+	( pilot_id int IDENTITY(1, 1),
+	  name varchar(25) NOT NULL,
+	  salary numeric(6, 2) NOT NULL check (salary > 0),
+	  primary key(pilot_id))
+	  ;
+
+	CREATE TABLE plane
+	( plane_id int IDENTITY(1,1),
+	  model varchar(10) NOT NULL,
+	  manufacturer varchar(20) NOT NULL,
+	  capacity int NOT NULL check(capacity > 0),
+	  primary key(plane_id));
+
+	CREATE TABLE schedule
+	( schedule_id int IDENTITY(1,1),
+	  plane_id int NOT NULL,
+	  route_id int NOT NULL,
+	  start_time datetime NOT NULL,
+	  end_time datetime NOT NULL,
+	  pilot_id int NOT NULL,
+	  status varchar(20) NOT NULL,
+	  primary key(schedule_id),
+	  foreign key(plane_id) references plane,
+	  foreign key(route_id) references route,
+	  foreign key(pilot_id) references pilot);
+
+	CREATE TABLE booking_info
+	( schedule_id int,
+	  class varchar(20) check(class in ('Economy', 'Business', 'First Class', 'Premium Economy')),
+	  price numeric(4, 2),
+	  primary key(schedule_id, class),
+	  foreign key(schedule_id) references schedule);
+
+	CREATE TABLE booking
+	( booking_id int IDENTITY(1, 1),
+	  schedule_id int NOT NULL,
+	  class varchar(20) NOT NULL check(class in ('Economy', 'Business', 'First Class', 'Premium Economy')),
+	  email varchar(25) NOT NULL,
+	  seat_num varchar(3) NOT NULL,
+	  primary key(booking_id),
+	  foreign key(schedule_id, class) references booking_info,
+	  foreign key(email) references passenger);
+  END
